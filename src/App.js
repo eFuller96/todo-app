38a03afc9f018ea936bcb4bc42acd3fd1d32cfe9
Ellie <eleanor.fuller@red-gate.com>
2020-02-07 15:38:08 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Header, Container } from 'semantic-ui-react'
+import './App.css';
+
+const App = (() => {
+  return <Container className='app'>
+        <Header className='app-header'>Todo App</Header>
+    </Container>
+})
+
+export default App;

585aa58be1c3a746e0105600e2dc73a78d555a7c
Ellie <eleanor.fuller@red-gate.com>
2020-02-14 17:48:15 +0000

    Add styling for adding product

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { Header, Container } from 'semantic-ui-react'
+import {
+  Header, Container, Form,
+  Button, Divider, Grid
+} from 'semantic-ui-react'
 import './App.css';
 
 const App = (() => {
-  return <Container className='app'>
-        <Header className='app-header'>Todo App</Header>
-    </Container>
+  return <Container className='page'>
+    <Header className='app-header'>Todo App</Header>
+    <Divider />
+    <Grid className='content'>
+      <Container className='add-item'>
+        <Form>
+          <Form.Field>
+            <label>Item Name</label>
+            <input placeholder='Item Name' />
+          </Form.Field>
+          <Button type='submit'>Add</Button>
+        </Form>
+      </Container>
+    </Grid>
+  </Container>
 })
 
 export default App;

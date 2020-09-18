169d2dfde6e4243c8adb6605664f78b2ee84c8cd
Ellie <eleanor.fuller@red-gate.com>
2020-09-18 17:32:40 +0100

    fixup guid

--- Todo.Web.Api/Controllers/ItemController.cs
+++ Todo.Web.Api/Controllers/ItemController.cs
@@ -36,6 +36,7 @@ namespace todo_app.Controllers
         [HttpPost]
         public IActionResult Post([FromBody] Item item)
         {
+            item.Id = Guid.NewGuid();
             _items.Add(item);
             return new JsonResult(_items);
         }
